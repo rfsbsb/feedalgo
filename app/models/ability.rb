20c50a185d70ebbec10259245f7a3c69ae680bb4
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2013-12-23 11:39:40 -0200

    Config for devise

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,12 +11,12 @@ class Ability
     #     can :read, :all
     #   end
     #
-    # The first argument to `can` is the action you are giving the user 
+    # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
     # here are :read, :create, :update and :destroy.
     #
-    # The second argument is the resource the user can perform the action on. 
+    # The second argument is the resource the user can perform the action on.
     # If you pass :all it will apply to every resource. Otherwise pass a Ruby
     # class of the resource.
     #
@@ -28,5 +28,10 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    if user.has_role? :admin
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
 end
