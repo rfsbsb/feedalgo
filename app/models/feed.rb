93023003e0fe29b33d035bd8bb20c389022757cf
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2013-12-23 12:38:20 -0200

    Moving things to the right place. Still have some TODOs

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,18 +1,3 @@
 class Feed < ActiveRecord::Base
-  attr_accessible :title, :url
-
-  def self.import
-    feeds = Feed.pluck(:url)
-    Feedzirra::Feed.fetch_and_parse(feeds, :on_success => lambda {|url, feed| Feed.import_success(url, feed)})
-  end
-
-  def self.import_success (url, import_feed)
-    feed = Feed.find_or_initialize_by_url(url)
-    if feed.new_record?
-      feed.title = import_feed.title
-      feed.save
-    end
-    FeedEntry.import(import_feed)
-  end
-
+  attr_accessible :title, :url, :favicon
 end
---
+++ lib/crawler.rb
@@ -0,0 +1,79 @@
+class Crawler
+  def run()
+    feeds = Feed.pluck(:url)
+    fetch(feeds)
+  end
+
+  def fetch(feeds)
+    Feedzirra::Feed.fetch_and_parse(feeds, :on_success => lambda {|url, feed| import(url, feed)})
+  end
+
+  def import(url, import_feed)
+    feed = Feed.find_or_initialize_by_url(url)
+    if feed.new_record?
+      feed.title = import_feed.title
+      feed.save
+    end
+    import_entry(import_feed)
+  end
+
+  def import_entry(feed)
+    feed.entries.each do |e|
+      entry = FeedEntry.find_or_initialize_by_url(e.url)
+      if entry.new_record?
+        body = nil
+        if e.respond_to?(:content) && !e.content.nil?
+          body = e.content
+        elsif e.respond_to?(:summary) && !e.summary.nil?
+          body = e.summary
+        end
+
+        entry.body = parse_body(body, feed)
+        entry.title  = e.title
+        entry.body = body
+        entry.author = e.author if e.author
+        entry.save
+      end
+    end
+  end
+
+  def parse_body(body, feed)
+    if body
+      # Getting the base URL of the site
+      uri = URI.parse(feed.url)
+      url = "#{uri.scheme}://#{uri.host}"
+      body = Sanitize.clean(body, Sanitize::Config::RELAXED)
+      doc = Nokogiri::HTML(body)
+      # Image parsing
+      doc.search("img").each do |img|
+        # Turning the image path absolute
+        if !img['src'].nil? && !img['src'].match(/^http/)
+          img['data-original'] = url + img['src']
+        else
+          img['data-original'] = img['src']
+        end
+        # Adding preloading data
+        img['src'] = "/assets/preloader.gif"
+        img['class'] = "img-polaroid img-rounded"
+      end
+
+      # Adding formatting classes for tables
+      doc.search("table").each do |table|
+        table['class'] = "table table-bordered table-striped table-condensed"
+      end
+
+      # Link parsing
+      doc.search("a").each do |link|
+        # Turning the link path absolute and opening in a new window
+        if !link['href'].nil? && !link['href'].match(/^http/)
+          link['href'] = url + link['href']
+        end
+        link['target'] = "_blank"
+      end
+
+      doc.xpath('//comment()').remove
+      doc.xpath("//body").to_html
+    end
+  end
+
+end
