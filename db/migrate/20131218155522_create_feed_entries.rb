e4505d56dd6522ad6f8cd0b66e2fbc86d8c626b6
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2014-03-11 09:42:09 -0300

    Fixed the size of url column to support loger URLs

--- db/migrate/20131218155522_create_feed_entries.rb
+++ db/migrate/20131218155522_create_feed_entries.rb
@@ -3,7 +3,7 @@ class CreateFeedEntries < ActiveRecord::Migration
     create_table :feed_entries do |t|
       t.string :title
       t.text :body
-      t.string :url
+      t.string :url, :limit => 500
       t.string :author
 
       t.timestamps
