24a5b83d583a2b9d579fa6782486fce61ebfa2d3
Rafael Silva <invites@rafaelsilva.net>
2020-11-08 12:21:05 -0500

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,29 +8,29 @@ Feedalgo::Application.routes.draw do
   resources  :folders,    :except  => ['update']
   resources  :feed_users, :except  => ['update']
 
-  match 'f/mark_as_read/:id'     => 'feed_entry_users#mark_as_read'
-  match 'f/favorite/:id'         => 'feed_entry_users#favorite'
+  get 'f/mark_as_read/:id'     => 'feed_entry_users#mark_as_read'
+  get 'f/favorite/:id'         => 'feed_entry_users#favorite'
   put   'folders/:id'            => "folders#remove",                        constraints: lambda {|req| req.params.has_key?(:remove)}
   put   'folders/:id'            => "folders#update",                        constraints: lambda {|req| req.params.has_key?(:save)}
   put   'feed_users/:id'         => "feed_users#unsubscribe",                constraints: lambda {|req| req.params.has_key?(:unsubscribe)}
   put   'feed_users/:id'         => "feed_users#update",                     constraints: lambda {|req| req.params.has_key?(:save)}
-  match 'f/import'               => 'feeds#import',                          :as => :import
-  match 'f/all'                  => 'feeds#all',                             :as => :reader_all
-  match 'f/paging/all'           => 'feeds#all_paging',                      :as => :reader_all_paging
-  match 'f/all/unread'           => 'feeds#all_unread',                      :as => :reader_all_unread
-  match 'f/all/favorites'        => 'feeds#all_favorite',                    :as => :reader_all_favorite
-  match 'f/paging/favorites'     => 'feeds#all_favorite_paging',             :as => :reader_all_favorite_paging
-  match 'f/folder/:id'           => 'folders#list',                          :as => :reader_folder
-  match 'f/folder/toggle/:id'    => 'folders#toggle',                        :as => :toggle_folder
-  match 'f/folder/unread/:id'    => 'folders#unread',                        :as => :folder_unread
-  match 'f/folder/favorites/:id' => 'folders#favorite',                      :as => :folder_favorite
-  match 'f/folder/paging/:id'    => 'folders#list_paging',                   :as => :folder_paging
-  match 'f/mark_all'             => 'feed_entry_users#mark_all_read',        :as => :mark_all,        :via => [:post]
-  match 'f/mark_all/:id'         => 'feed_entry_users#mark_all_feed_read',   :as => :mark_all_feed,   :via => [:post], :constraints => { :id => /[^\/]*/ }
-  match 'f/folder/mark_all/:id'  => 'feed_entry_users#mark_all_folder_read', :as => :mark_all_folder, :via => [:post], :constraints => { :id => /[^\/]*/ }
-  match 'f/unread/:id'           => 'feeds#unread',                          :as => :unread,          :constraints => { :id => /[^\/]*/ }
-  match 'f/favorites/:id'        => 'feeds#favorite',                        :as => :favorite,        :constraints => { :id => /[^\/]*/ }
-  match 'f/paging/:id'           => 'feeds#list_paging',                     :as => :feed_paging,     :constraints => { :id => /[^\/]*/ }
-  match 'f/:id'                  => 'feeds#list',                            :as => :reader_feed,     :constraints => { :id => /[^\/]*/ }
+  get 'f/import'               => 'feeds#import',                          :as => :import
+  get 'f/all'                  => 'feeds#all',                             :as => :reader_all
+  get 'f/paging/all'           => 'feeds#all_paging',                      :as => :reader_all_paging
+  get 'f/all/unread'           => 'feeds#all_unread',                      :as => :reader_all_unread
+  get 'f/all/favorites'        => 'feeds#all_favorite',                    :as => :reader_all_favorite
+  get 'f/paging/favorites'     => 'feeds#all_favorite_paging',             :as => :reader_all_favorite_paging
+  get 'f/folder/:id'           => 'folders#list',                          :as => :reader_folder
+  get 'f/folder/toggle/:id'    => 'folders#toggle',                        :as => :toggle_folder
+  get 'f/folder/unread/:id'    => 'folders#unread',                        :as => :folder_unread
+  get 'f/folder/favorites/:id' => 'folders#favorite',                      :as => :folder_favorite
+  get 'f/folder/paging/:id'    => 'folders#list_paging',                   :as => :folder_paging
+  get 'f/mark_all'             => 'feed_entry_users#mark_all_read',        :as => :mark_all,        :via => [:post]
+  get 'f/mark_all/:id'         => 'feed_entry_users#mark_all_feed_read',   :as => :mark_all_feed,   :via => [:post], :constraints => { :id => /[^\/]*/ }
+  get 'f/folder/mark_all/:id'  => 'feed_entry_users#mark_all_folder_read', :as => :mark_all_folder, :via => [:post], :constraints => { :id => /[^\/]*/ }
+  get 'f/unread/:id'           => 'feeds#unread',                          :as => :unread,          :constraints => { :id => /[^\/]*/ }
+  get 'f/favorites/:id'        => 'feeds#favorite',                        :as => :favorite,        :constraints => { :id => /[^\/]*/ }
+  get 'f/paging/:id'           => 'feeds#list_paging',                     :as => :feed_paging,     :constraints => { :id => /[^\/]*/ }
+  get 'f/:id'                  => 'feeds#list',                            :as => :reader_feed,     :constraints => { :id => /[^\/]*/ }
 
 end
